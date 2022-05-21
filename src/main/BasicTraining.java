fc6e3c79273483163c5bf4d3774876c70ba83553
simonsimon9 <simonkuang9@outlook.com>
2022-05-21 18:59:35 -0400

    starter file part 2

--- src/main/BasicTraining.java
+++ src/main/BasicTraining.java
@@ -12,7 +12,15 @@ public class BasicTraining {
 	 * @return a sorted array
 	 */
 	public static <T extends Comparable<? super T>> T[] quickSort(T[] array) {
+		
+		
 		return null;
 	}
+	
+	//pick pivot - pick middle. 
+	//Swap last element with pivot.
+	//start with two pointers. - one from swapping, one for incrementing. 
+	
+	
 	
 }
