9a1f4b83c9cb38fc77ded78e502a0ad0e35361ac
simonsimon9 <simonkuang9@outlook.com>
2022-05-25 10:46:35 -0400

    added sorting code to basic training, still not functional

--- src/main/BasicTraining.java
+++ src/main/BasicTraining.java
@@ -11,16 +11,98 @@ public class BasicTraining {
 	 * @param array
 	 * @return a sorted array
 	 */
-	public static <T extends Comparable<? super T>> T[] quickSort(T[] array) {
+	public static <T extends Comparable<? super T>> T[] quickSort(T[] a) {
 		
+		int middle = (a.length - 1) / 2;
+		int last = (a.length - 1);
 		
-		return null;
+		swap(a, middle, last);
+		int pivot = last;
+		
+		int left = 0;
+		int right = last - 1;
+		
+		while(left < right) {
+			if(a[left].compareTo(a[pivot]) < 0 ) {
+				left++;
+			}
+			
+			if(a[right].compareTo(a[pivot]) > 0){
+				right--;
+			}
+			
+			if(a[left].compareTo(a[pivot]) > 0 && a[right].compareTo(a[pivot]) < 0) {
+				swap(a, left, right);
+				left++;
+				right--;
+			}
+			
+		}
+		
+		swap(a, pivot, left);
+		
+		//T temp = array[last];
+		//array[last] = array[middle];
+		//array[middle] = temp;
+		
+		return a;
 	}
 	
-	//pick pivot - pick middle. 
-	//Swap last element with pivot.
-	//start with two pointers. - one from swapping, one for incrementing. 
+	public static  <T extends Comparable<? super T>> void swap(T[] array, int firstIndex , int secondIndex) {
+		T temp = array[firstIndex];
+		array[firstIndex] = array[secondIndex];
+		array[secondIndex] = temp;
+		
+		//passes by reference, returns by reference
+		
+	}
 	
-	
+	public static <T extends Comparable<? super T>> T[] partition(T[] a, int firstIndex, int lastIndex) {
+		int middle = (lastIndex + firstIndex)/ 2;
+		int last = lastIndex;
+		
+		swap(a, middle, last);
+		int pivot = last;
+		
+		int left = firstIndex;
+		int right = pivot - 1;
+		
+		while(left < right) {
+			if(a[left].compareTo(a[pivot]) < 0 ) {
+				left++;
+			}
+			
+			if(a[right].compareTo(a[pivot]) > 0){
+				right--;
+			}
+			
+			if(a[left].compareTo(a[pivot]) > 0 && a[right].compareTo(a[pivot]) < 0) {
+				swap(a, left, right);
+				System.out.println("swapped");
+				left++;
+				right--;
+			}
+			print(a, pivot, left, right);
+		}
+		
+		swap(a, pivot, left);
+		
+		//T temp = array[last];
+		//array[last] = array[middle];
+		//array[middle] = temp;
+		
+		return a;
+		
+	}
+	public static <T extends Comparable<? super T>> void print(T[] array, int pivot, int first, int last) {
+		
+		for(T element: array){
+			System.out.print(element + " ");
+		}
+		System.out.print("    pivot is at: " + pivot +
+						 " left is at: " +first+
+						 " right is at: " + last);
+		System.out.println("\n-------------");
+	}
 	
 }
--- src/testing/BasicTrainingTest.java
+++ src/testing/BasicTrainingTest.java
@@ -4,11 +4,49 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import main.BasicTraining;
+
 public class BasicTrainingTest {
 
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSwap() {
+		Integer[] test = new Integer[] {2,6,5,3,8,7,1,0};
+		Integer[] expectedTest = new Integer[] {2,6,5,0,8,7,1,3};
+		int last = test.length-1;
+		int middle=  (test.length - 1) / 2;
+		
+		BasicTraining.swap(test, middle, last);
+		
+		assertArrayEquals(test,expectedTest);
 	}
-
+	
+	@Test
+	public void testPartition() {
+		Integer[] test = new Integer[] {2,6,5,3,8,7,1,0};
+		Integer[] expectedTest = new Integer[] {2,1,0,3,8,7,6,5};
+		int first = 0;
+		int last = test.length - 1;
+		BasicTraining.partition(test, first, last);
+		assertArrayEquals(test,expectedTest);
+		
+	}
+	
+	@Test
+	public void testPartition0to4() {
+		Integer[] test = new Integer[] {2,1,0,3,8,7,6,5};
+		Integer[] expectedTest = new Integer[] {2,1,0,3,6,5,7,8};
+	
+		int first = 4;
+		int last = (test.length - 1);
+		BasicTraining.partition(test, first, last);
+		for(int element: test) {
+			System.out.print(element + " ");
+		}
+		System.out.println("");
+		for(int element: expectedTest){
+			System.out.print(element + " ");
+		}
+		assertArrayEquals(test,expectedTest);
+	}
 }
