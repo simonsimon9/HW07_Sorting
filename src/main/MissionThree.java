17f9f5cdb17a0980e893b73bbddd7b7af819d9f0
simonsimon9 <simonkuang9@outlook.com>
2022-05-27 17:53:23 -0400

    Finished mission3

--- src/main/MissionThree.java
+++ src/main/MissionThree.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.ArrayList;
+
 public class MissionThree {
 
 	/**
@@ -10,7 +12,48 @@ public class MissionThree {
 	 * @return the difference in score of person1 and person2
 	 */
 	public static <T extends Comparable<? super T>> int merge(T[] person1, T[] person2) {
-		return 0;
+		T[] mergedArray = (T[]) new Comparable[person1.length+person2.length];
+		int indexMArray = 0;
+		int indexPerson1 = 0;
+		int indexPerson2 = 0;
+		int tallyPerson1 = 0;
+		int tallyPerson2 = 0;
+		while(indexPerson1 != person1.length && indexPerson2 != person2.length) {
+			
+			if(person1[indexPerson1].compareTo(person2[indexPerson2]) < 0) {
+				mergedArray[indexMArray] = person1[indexPerson1];
+				tallyPerson1 = tallyPerson1 + indexMArray + 1;
+				indexMArray++;
+				indexPerson1++;
+				
+			}else if(person1[indexPerson1].compareTo(person2[indexPerson2]) > 0) {
+				mergedArray[indexMArray] = person2[indexPerson2];
+				tallyPerson2 = tallyPerson2 + indexMArray + 1;
+				indexMArray++;
+				indexPerson2++;
+			}
+			
+		}
+		
+		if(indexPerson1 == person1.length) {
+			
+			for(int i = indexPerson2; i < person2.length; i++) {
+				mergedArray[indexMArray] = person2[i];
+				tallyPerson2 = tallyPerson2 + indexMArray + 1;
+				indexMArray++;
+			}
+		}else if(indexPerson2 == person2.length) {
+			for(int i = indexPerson1; i < person1.length; i++) {
+				mergedArray[indexMArray] = person1[i];
+				tallyPerson1 = tallyPerson1 + indexMArray + 1;
+				indexMArray++;
+			}
+		}
+		
+
+		return tallyPerson1 - tallyPerson2;
+		
+		
 	}
 	
 }
--- src/testing/MissionThreeTest.java
+++ src/testing/MissionThreeTest.java
@@ -1,5 +1,22 @@
 package testing;
 
-public class MissionThreeTest {
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+import main.MissionOne;
+import main.MissionThree;
+
+public class MissionThreeTest {
+	@Test
+	public void test() {
+		Integer[] test = new Integer[] {1,2,3,5,8,10};
+		Integer[] expectedTest = new Integer[] {4,6,7,9};
+
+		MissionThree.merge(test, expectedTest);
+		assertEquals(MissionThree.merge(test, expectedTest), 3);
+
+
+	}
 }
